8173d8399ff4e1616a972ea72a8544016812c78d
work pc <work pc>
2024-12-04 17:01:58 +0300

    update

--- cnc_figure_mover.py
+++ cnc_figure_mover.py
@@ -1,80 +1,87 @@
-file_path = '/home/slippa/gitrep/utilities/'
-file_name = 'file1.8'
+file_path = 'C:\\Users\\Administrator\\PythonProgs\\test\\lidsorig\\'
+file_path_ch = 'C:\\Users\\Administrator\\PythonProgs\\test\\lids\\'
+file_name = '1.7'
 postfix = '_changed_copy'
 file_end = '.tap'
-g_begin = 4
-g_end = 19
-g_div = 39
-g_count = 4
+
+
+def cnc_move(commands, ch_x, ch_y, begin, end):
+    k = 0
+    for command in commands:
+        index = 0
+        xb = 0
+        xe = 0
+        yb = 0
+        ye = 0
+        if begin <= k <= end:
+            print(str(begin) + ' ' + str(end))
+            for letter in command:
+                if letter == 'X':
+                    xb = index
+                    break
+                index += 1
+            for j in range(xb + 1, len(command)):
+                if command[j].isdigit() or command[j] == '.':
+                    index += 1
+                    if j + 1 == len(command):
+                        xe = index
+                else:
+                    xe = index
+                    break
+
+            if not (xb == 0 or xe == 0):
+                print(command[xb:xe + 1])
+                orig_x = float(command[xb + 1:xe + 1])
+                command = command.replace(command[xb:xe + 1], 'X' + "{:.3f}".format(orig_x + ch_x))
+
+            index = 0
+            for letter in command:
+                if letter == 'Y':
+                    yb = index
+                    break
+                index += 1
+            for j in range(yb + 1, len(command)):
+                if command[j].isdigit() or command[j] == '.':
+                    index += 1
+                    if j + 1 == len(command):
+                        ye = index
+                else:
+                    ye = index
+                    break
+
+            if not (yb == 0 or ye == 0):
+                print(command[yb:ye + 1])
+                orig_y = float(command[yb + 1:ye + 1])
+                command = command.replace(command[yb:ye + 1], 'Y' + "{:.3f}".format(orig_y + ch_y))
+
+            commands[k] = command
+            print(str(xb) + ' ' + str(xe) + ' ' + str(yb) + ' ' + str(ye))
+            print(command)
+        k += 1
+
+
 g_x_ch_val = 58
 g_y_ch_val = 10
 
+# [begin value, end value]
+be_arr = [[3, 16], [43, 56], [82, 95], [121, 135], [162, 176]]
 
-def cnc_move(commands, ch_x, ch_y, begin, end, div, count_begin, count, minus_val):
-    for i in range(count_begin, count - 1):
-        k = 0
-        for command in commands:
-            index = 0
-            xb = 0
-            xe = 0
-            yb = 0
-            ye = 0
-            if begin + i * div <= k <= end + i * div:
-                print(str(begin + i * div) + ' ' + str(end + i * div))
-                for letter in command:
-                    if letter == 'X':
-                        xb = index
-                        break
-                    index += 1
-                for j in range(xb + 1, len(command)):
-                    if command[j].isdigit() or command[j] == '.':
-                        index += 1
-                        if j + 1 == len(command):
-                            xe = index
-                    else:
-                        xe = index
-                        break
-
-                if not (xb == 0 or xe == 0):
-                    print(command[xb:xe + 1])
-                    orig_x = float(command[xb + 1:xe + 1])
-                    minus = minus_val
-                    if i % 2 == 0:
-                        minus = -1 * minus_val
-                    command = command.replace(command[xb:xe + 1], 'X' + "{:.3f}".format(orig_x + ch_x * minus))
-
-                index = 0
-                for letter in command:
-                    if letter == 'Y':
-                        yb = index
-                        break
-                    index += 1
-                for j in range(yb + 1, len(command)):
-                    if command[j].isdigit() or command[j] == '.':
-                        index += 1
-                        if j + 1 == len(command):
-                            ye = index
-                    else:
-                        ye = index
-                        break
-
-                if not (yb == 0 or ye == 0):
-                    print(command[yb:ye + 1])
-                    orig_y = float(command[yb + 1:ye + 1])
-                    command = command.replace(command[yb:ye + 1], 'Y' + "{:.3f}".format(orig_y + ch_y))
-
-                commands[k] = command
-                print(str(xb) + ' ' + str(xe) + ' ' + str(yb) + ' ' + str(ye))
-                print(command)
-            k += 1
+g_begin = be_arr[0][0]
+g_end = be_arr[0][1]
 
 g_file = open(file_path + file_name + file_end)
-g_ch_f = open(file_path + file_name + postfix + file_end, "w+")
-commands_full = g_file.read()
-g_commands = commands_full.split('\n')
-cnc_move(g_commands, g_x_ch_val, g_y_ch_val, g_begin, g_end, g_div, 0, 2, -1)
-g_begin = 45
-g_end = 58
-cnc_move(g_commands, g_x_ch_val, g_y_ch_val, g_begin, g_end, g_div, 0, 4, 1)
-g_ch_f.write('\n'.join(g_commands))
+with open(file_path_ch + file_name + postfix + file_end, "w+") as g_ch_f:
+    commands_full = g_file.read()
+    g_commands = commands_full.split('\n')
+    count = 0
+    for range_ in be_arr:
+        g_begin = range_[0]
+        g_end = range_[1]
+        x = g_x_ch_val
+        
+        if count % 2 == 1:
+            x = g_x_ch_val * -1
+        cnc_move(g_commands, x, g_y_ch_val, g_begin, g_end)
+        count += 1
+    g_ch_f.write('\n'.join(g_commands))
 g_file.close()
--- utilt.py
+++ utilt.py
@@ -13,52 +13,51 @@ def ras (x1, y1, x2, y2, x3, y3):
     dl=math.sqrt((x3-xz)*(x3-xz)+(y3-xz*k-d)*(y3-xz*k-d))
   return dl
 
-k = 3
+def segment_dist(xa, ya, xb, yb, xc, yc, xd, yd):
 
-xa, ya, xb, yb = [1419, 110 + k, 168, 231 + k]
-xc, yc, xd, yd = [59, 189, 1380, 61]
+  min_t=-1
+  t=-2
+  s=-2
 
-# xa, ya, xb, yb = [1419, 545 + k, 168, 424 + k]
-# xc, yc, xd, yd = [59, 472, 1380, 600]
+  o=(xb-xa)*(-yd+yc)-(yb-ya)*(-xd+xc)
+  o1=(xb-xa)*(yc-ya)-(yb-ya)*(xc-xa)
+  o2=(-yd+yc)*(xc-xa)-(-xd+xc)*(yc-ya)
 
-min_t=-1
-t=-2
-s=-2
+  if o!=0:
+    t=o1/o
+    s=o2/o
 
-o=(xb-xa)*(-yd+yc)-(yb-ya)*(-xd+xc)
-o1=(xb-xa)*(yc-ya)-(yb-ya)*(xc-xa)
-o2=(-yd+yc)*(xc-xa)-(-xd+xc)*(yc-ya)
+  if (t>=0 and s>=0) and (t<=1 and s<=1):
+    min_t=0
+  else:
 
-if o!=0:
-  t=o1/o
-  s=o2/o
-
-if (t>=0 and s>=0) and (t<=1 and s<=1):
-  min_t=0
-else:
-
-  dl1=ras(xa,ya,xb,yb,xc,yc)
-  min_t=dl1
-  dl2=ras(xa,ya,xb,yb,xd,yd)
-  if (dl2 < min_t and dl2 != -1) or min_t==-1 :
-    min_t=dl2
-  dl3=ras(xc,yc,xd,yd,xa,ya)
-  if (dl3 < min_t and dl3 != -1) or min_t==-1 :
-    min_t=dl3
-  dl4=ras(xc,yc,xd,yd,xb,yb)
-  if (dl4 < min_t and dl4 != -1) or min_t==-1 :
-    min_t=dl4
-  if min_t==-1 :
-    dl1=math.sqrt((xa-xc)*(xa-xc)+(ya-yc)*(ya-yc))
+    dl1=ras(xa,ya,xb,yb,xc,yc)
     min_t=dl1
-    dl2=math.sqrt((xb-xd)*(xb-xd)+(yb-yd)*(yb-yd))
-    if dl2<min_t :
+    dl2=ras(xa,ya,xb,yb,xd,yd)
+    if (dl2 < min_t and dl2 != -1) or min_t==-1 :
       min_t=dl2
-    dl3=math.sqrt((xb-xc)*(xb-xc)+(yb-yc)*(yb-yc))
-    if dl3<min_t :
+    dl3=ras(xc,yc,xd,yd,xa,ya)
+    if (dl3 < min_t and dl3 != -1) or min_t==-1 :
       min_t=dl3
-    dl4=math.sqrt((xa-xd)*(xa-xd)+(ya-yd)*(ya-yd))
-    if dl4<min_t :
+    dl4=ras(xc,yc,xd,yd,xb,yb)
+    if (dl4 < min_t and dl4 != -1) or min_t==-1 :
       min_t=dl4
+    if min_t==-1 :
+      dl1=math.sqrt((xa-xc)*(xa-xc)+(ya-yc)*(ya-yc))
+      min_t=dl1
+      dl2=math.sqrt((xb-xd)*(xb-xd)+(yb-yd)*(yb-yd))
+      if dl2<min_t :
+        min_t=dl2
+      dl3=math.sqrt((xb-xc)*(xb-xc)+(yb-yc)*(yb-yc))
+      if dl3<min_t :
+        min_t=dl3
+      dl4=math.sqrt((xa-xd)*(xa-xd)+(ya-yd)*(ya-yd))
+      if dl4<min_t :
+        min_t=dl4
 
-print (min_t)
+  print (min_t)
+
+  return min_t
+
+
+
